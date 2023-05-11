b1be86f17f47309d84ae30f82f8445d20b150001
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-05-11 12:04:38 +0200

    Add http constants

---
+++ src/app/http/http-constants.ts
@@ -0,0 +1,8 @@
+export class HttpConstants {
+    static posts = 'posts/';
+    static comments = 'comments/';
+    static albums = 'albums/';
+    static photos = 'photos/';
+    static todos = 'todos/';
+    static users = 'users/';
+  }
