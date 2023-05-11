65933f31ac9f6a94973017c96652fa999a927d86
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-05-11 12:03:45 +0200

    Add navbar component

--- src/app/features/layout/components/navbar/navbar.component.ts
+++ src/app/features/layout/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  
+  router = inject(Router);
 
+  logout() {
+    this.router.navigateByUrl('auth/login');
+  }
 }
